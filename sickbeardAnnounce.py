5e10be7753f6acf84750e2a78d9647bf7d285b63
Dion <woolley.dion@gmail.com>
2015-02-23 07:30:08 +1300

    debugging

--- sickbeardAnnounce.py
+++ sickbeardAnnounce.py
@@ -3,7 +3,5 @@ import subprocess
 
 filePath = sys.argv[1]
 airDate = sys.argv[6]
-print(filePath)
-print(airDate)
 
-subprocess.call("nodejs ~/DCPlusPlusAnnouncer/src/main/rlsBot.js " + str(filePath) + " " + str(airDate), shell=True)
+subprocess.call("nodejs ~/DCPlusPlusRlsAnnouncer/src/main/rlsBot.js " + str(filePath) + " " + str(airDate), shell=True)
