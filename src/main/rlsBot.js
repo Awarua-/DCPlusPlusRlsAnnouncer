d88367af76591637888999d96673b32e79ed52ff
Dion <woolley.dion@gmail.com>
2015-02-23 07:52:10 +1300

    debugging

--- src/main/rlsBot.js
+++ src/main/rlsBot.js
@@ -51,7 +51,7 @@ function getFileSize() {
 
 function getTTH(callback) {
     var spawn = require('child_process').spawn;
-    var prc = spawn('java',  ['-jar', '../../lib/tth/tth.jar', filePath]);
+    var prc = spawn('java',  ['-jar', '../../lib/tth/tth.jar', "\'" +filePath + "\'"]);
     console.log(filePath);
 
 //noinspection JSUnresolvedFunction
