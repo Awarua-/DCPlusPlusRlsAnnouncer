5510f3d2ba5d37351038b031c8d13d36ff3108c4
Dion <woolley.dion@gmail.com>
2015-02-28 08:06:23 +1300

    updated couchpotato support

--- CouchPotato/PostProcess/main.py
+++ CouchPotato/PostProcess/main.py
@@ -21,10 +21,27 @@ class PostProcess(Plugin):
 
         command = ['nodejs', '~/DCPlusPlusRlsAnnouncer/src/main/rlsBot.js']
         for x in moviefile:
-            print(x)
+            log.info('moviefile' +str(x))
             command.append(x)
 
         command.append(date)
+
+
+        log.info('Command TIME, SON!')
+        log.info('files='+str(group['files']))
+        log.info('destination_dir='+str(group['destination_dir']))
+        log.info('identifier='+str(group['identifier']))
+        log.info('media='+str(group['media']))
+        log.info('release_download='+str(group['release_download']))
+        log.info('identifiers='+str(group['identifiers']))
+        log.info('is_dvd='+str(group['is_dvd']))
+        log.info('filename='+str(group['filename']))
+        log.info('meta_data='+str(group['meta_data']))
+        log.info('parentdir='+str(group['parentdir']))
+        log.info('renamed_files='+str(group['renamed_files']))
+        log.info('before_rename='+str(group['before_rename']))
+        log.info('dirname='+str(group['dirname']))
+        log.info('subtitle_language='+str(group['subtitle_language']))
 
         # try:
         #     p = Popen(command, stdout=PIPE)
