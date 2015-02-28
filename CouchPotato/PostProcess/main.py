25c51c4dca810a0ccf96eb7151b5b53ead3ad1c0
Dion <woolley.dion@gmail.com>
2015-03-01 03:07:00 +1300

    updated couchpotato support

--- CouchPotato/PostProcess/main.py
+++ CouchPotato/PostProcess/main.py
@@ -4,7 +4,7 @@ from couchpotato.core.plugins.base import Plugin
 from subprocess import Popen, PIPE
 import traceback
 from datetime import datetime
-from time import strftime
+from os import path
 
 log = CPLog(__name__)
 
@@ -21,38 +21,44 @@ class PostProcess(Plugin):
 
         command = ['nodejs', '~/DCPlusPlusRlsAnnouncer/src/main/rlsBot.js']
         for x in moviefile:
-            log.info('moviefile' +str(x))
-            command.append(x)
+            log.info('moviefile' + str(x))
 
+            if str(x).endswith(".mkv") or str(x).endswith(".avi") or str(x).endswith(".mp4"):
+                command.append(x)
+
+        if len(command) == 0:
+            return False
+
+        if len(command) == 1:
+            movie = command[0]
+
+        if len(command) > 1:
+            movie = self.find_largest_file(command)
+
+        command.append(movie)
         command.append(date)
 
 
-        log.info('Command TIME, SON!')
-        log.info('files='+str(group['files']))
-        log.info('destination_dir='+str(group['destination_dir']))
-        log.info('identifier='+str(group['identifier']))
-        log.info('media='+str(group['media']))
-        log.info('release_download='+str(group['release_download']))
-        log.info('identifiers='+str(group['identifiers']))
-        log.info('is_dvd='+str(group['is_dvd']))
-        log.info('filename='+str(group['filename']))
-        log.info('meta_data='+str(group['meta_data']))
-        log.info('parentdir='+str(group['parentdir']))
-        log.info('renamed_files='+str(group['renamed_files']))
-        log.info('before_rename='+str(group['before_rename']))
-        log.info('dirname='+str(group['dirname']))
-        log.info('subtitle_language='+str(group['subtitle_language']))
+        try:
+            p = Popen(command, stdout=PIPE)
+            res = p.wait()
+            if res == 0:
+                log.info('PostProcess Script was called successfully')
+                return True
+            else:
+                log.info('PostProcess Script returned an error code: %s', str(res))
 
-        # try:
-        #     p = Popen(command, stdout=PIPE)
-        #     res = p.wait()
-        #     if res == 0:
-        #         log.info('PostProcess Script was called successfully')
-        #         return True
-        #     else:
-        #         log.info('PostProcess Script returned an error code: %s', str(res))
-        #
-        # except:
-        #     log.error('Failed to call script: %s', (traceback.format_exc()))
+        except:
+            log.error('Failed to call script: %s', (traceback.format_exc()))
 
         return False
+
+    def find_largest_file(self, files):
+        largest = path.getsize(files[0])
+
+        for movie in files[1:]:
+            current = path.getsize(movie)
+            if current > largest:
+                largest = current
+
+        return largest
