4c7061c9a7af8f6c2299921bbb1412644a2bd34c
Dion Woolley <woolley.dion@gmail.com>
2016-08-13 22:42:02 +1200

    Fixed subprocess call

--- intergrations/SickBeard/sickbeardAnnounce.py
+++ intergrations/SickBeard/sickbeardAnnounce.py
@@ -4,4 +4,4 @@ import subprocess
 filePath = sys.argv[1]
 airDate = sys.argv[6]
 
-subprocess.call("node ~/DCPlusPlusRlsAnnouncer/dcplusplusreleaseannouncer.js \"" + str(filePath) + "\" \"" + str(airDate) + "\"", shell=True)
+subprocess.call(["node", "~/DCPlusPlusRlsAnnouncer/dcplusplusreleaseannouncer.js", str(filePath), str(airDate)], shell=True)
